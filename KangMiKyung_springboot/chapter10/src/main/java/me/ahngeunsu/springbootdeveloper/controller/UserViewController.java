0f197e9434dbf59553a3fc280aa034b97d97aa19
Mi-Kyung331 <uu.3120@gmail.com>
2025-01-20 21:43:30 +0900

    feat: 20250120

--- KangMiKyung_springboot/chapter10/src/main/java/me/ahngeunsu/springbootdeveloper/controller/UserViewController.java
+++ KangMiKyung_springboot/chapter10/src/main/java/me/ahngeunsu/springbootdeveloper/controller/UserViewController.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserViewController {
+    // 기존 로그인 페이지
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
+
+    // oauth 관련 로그인 페이지
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "oauthLogin";
     }
+    // https://developers.google.com/identity/branding-guidelines?hl=ko 여기서 다운받을 수 있음
 
     @GetMapping("/signup")
     public String signup() {
--- KangMiKyung_springboot/chapter10/src/main/java/me/ahngeunsu/springbootdeveloper/dto/ArticleListViewResponse.java
+++ KangMiKyung_springboot/chapter10/src/main/java/me/ahngeunsu/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -3,16 +3,22 @@ package me.ahngeunsu.springbootdeveloper.dto;
 import lombok.Getter;
 import me.ahngeunsu.springbootdeveloper.domain.Article;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ArticleListViewResponse {
     private final Long id;
     private final String title;
     private final String content;
+    private LocalDateTime createdAt;
+    private String author;
 
     public ArticleListViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+        this.author = article.getAuthor();
     }
 
 }
